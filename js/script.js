87a09ffd25dff2084db26ddc5effe0e8a75dfbf8
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-09-30 14:50:40 -0700

    update practica4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+   // alert("hola mundo");
      function getdata() {
         var dato1=document.getElementById("dato1").value;
         var mul=document.getElementById("multiplicador").value;
@@ -7,4 +7,11 @@
 
     
         datos.innerHTML=" su resultado es "+total;
+
     }
+    // var esteesunstring="este es un string";
+    // console.log(esteesunstring.length);
+    // console.log(esteesunstring.split(' '));
+    // var numero=23.5;
+    // var enteros=2;
+    // console.log(23.5*2);
